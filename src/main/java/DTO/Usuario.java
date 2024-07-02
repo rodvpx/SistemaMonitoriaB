e03d9e57accd453b29ac56f01d836f3a4aab65e7
Rodrigo <rodrigoip435@gmail.com>
2024-07-02 08:46:08 -0300

    oi

--- src/main/java/DTO/Usuario.java
+++ src/main/java/DTO/Usuario.java
@@ -22,42 +22,6 @@ public abstract class Usuario {
 
     protected abstract boolean login();
 
-
-		/*
-		//antigo metodo cadastrar usuario
-		if (!validar()) {
-			System.out.println("Erro: Informações inválidas.");
-			return;
-		}
-	
-		// Verifica se o e-mail ou matrícula já estão sendo usados
-		for (Usuario usuario : usuarios) {
-			if (usuario.getEmail().equals(this.email)) {
-				System.out.println("Erro: Este e-mail já está cadastrado.");
-				return;
-			}
-			if (usuario.getMatricula().equals(this.matricula)) {
-				System.out.println("Erro: Esta matrícula já está cadastrada.");
-				return;
-			}
-		}
-	
-		// Se passou pelas verificações, adiciona o novo usuário
-		usuarios.add(this);
-		System.out.println("Usuário cadastrado com sucesso.");*/
-
-
-		/*
-		//antigo metodo de login
-		if (this.email.equals(email) && this.senha.equals(senha)) {
-			System.out.println("Login realizado com sucesso.");
-			return true;
-		} else {
-			System.out.println("Email ou senha incorretos. Tente novamente.");
-			return false;
-		}*/
-
-
     public String getNome() {
         return nome;
     }
