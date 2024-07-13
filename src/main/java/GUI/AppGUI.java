2166990eb7aa9c6b35fc020fb662791eb1b32117
Kaue <kaue.rosa.prof@gmail.com>
2024-07-13 15:29:26 -0300

    AppGUI nova

--- src/main/java/GUI/AppGUI.java
+++ src/main/java/GUI/AppGUI.java
@@ -24,7 +24,7 @@ public class AppGUI extends JFrame {
 
     public AppGUI() {
         setTitle("Sistema de Monitoria");
-        setSize(400, 200);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         inicializarTelaPrincipal();
